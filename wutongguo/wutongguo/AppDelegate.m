85adc5afaefe76c81e3c4c1b083a4ee2034cbd0a
madyson <766799316@qq.com>
2019-02-18 09:13:11 +0800

    更新

--- wutongguo/wutongguo/AppDelegate.m
+++ wutongguo/wutongguo/AppDelegate.m
@@ -42,7 +42,6 @@
         NSData *mainBundleFile = [NSData dataWithContentsOfFile:originDbPath];
         [file createFileAtPath:dbPath contents:mainBundleFile attributes:nil];
     }
-    
     /**
      *  设置ShareSDK的appKey，如果尚未在ShareSDK官网注册过App，请移步到http://mob.com/login 登录后台进行应用注册，
      *  在将生成的AppKey传入到此方法中。
